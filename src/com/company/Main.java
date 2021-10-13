193beb8e371460fa7d250623ff2b967497410f24
SaraEngsted <sara.andreasen@live.dk>
2021-10-13 12:08:36 +0200

    Hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,22 @@ public class Main {
     public static void main(String[] args) {
 
         //Pizzaer
+
         Pizza vesuvio = new Pizza("Vesuvio", 59, true, true, "Skinke", "", "", true, "", "", "");
         Pizza magarita = new Pizza("Magarita",59,true,true,"","","",true,"","","");
         Pizza bertil = new Pizza("Betil",59,true,true,"Bacon","","",true,"","","");
         Pizza victoria = new Pizza("Victoria",75,true,true,"Skinke","","",true,"Champignon","Løg","Ananas");
+
+
+        ArrayList<String> listOfPizzaToppings = new ArrayList<String>();
+        listOfPizzaToppings.add("Tomatsauce");
+        listOfPizzaToppings.add("Ost");
+        listOfPizzaToppings.add("Oregano");
+        listOfPizzaToppings.add("Skinke");
+        listOfPizzaToppings.add("Bacon");
+        listOfPizzaToppings.add("Løg");
+        listOfPizzaToppings.add("Champingnon");
+        listOfPizzaToppings.add("Ananas");
 
 
 
